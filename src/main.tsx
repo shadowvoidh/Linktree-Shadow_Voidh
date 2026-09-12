11fc770593c3bd9d0f869bf8b7c13ee13545807d
Pedro Carnio | Shadow_Voidh <shadow.voidh@gmail.com>
2026-09-12 01:23:57 -0300

    fix: adiciona base no vite.config e HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import PersonalLinktree from "./pages/PersonalLinktree";
 import NotFoundPage from "./pages/NotFoundPage";
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Linktree-Shadow_Voidh/',
+ base: '/Linktree-Shadow_Voidh/',
 });
